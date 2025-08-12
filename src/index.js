93e9ab437851b1a80ad02b9ec8bf9a623623a43d
Kush Gupta <Kushg827@gmail.com>
2025-08-12 20:51:32 +0530

    Added create and delete city function in city repository

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig.js");
+// const { CityRepository } = rfequire("./repository/index.js");
+// const { City } = require("./models/index.js")
+
 
 const setupAndStartServer = async () => {
   const app = express();
@@ -7,8 +10,15 @@ const setupAndStartServer = async () => {
   app.use(express.json());
   app.use(express.urlencoded({extended: true}));
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server is running at port ${PORT}`);
+    // await City.create({
+    //   name: "New Delhi"
+    // })
+
+    // const repo = new CityRepository();
+    // repo.deleteCity(1);
+    
   });
 };
 
